d4000324456e6b1e9c65e8a5957f31d497cdeab6
chitsanu345 <chitsanu345@gmail.com>
2017-10-05 20:51:16 +0700

    P03

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,27 @@
 ﻿using System;
 
 namespace ConsoleApp1
+{
+    class Student
+    {
+        private string name;
+        private string id;
+        private float gpa;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public float GPA
+        {
+            get { return gpa; }
+            set { gpa = value; }
+        }
+    }
+
+}
