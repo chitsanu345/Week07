fdd0075f2883724c8696c88914cc9403e59f4f48
chitsanu345 <chitsanu345@gmail.com>
2017-10-05 20:44:44 +0700

    P01

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,26 @@
 ﻿using System;
 
 namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student su = new Student();
+            su.Name = "Student Name";
+            su.ID = "12345678";
+            su.GPA = 3.5f;
+            Console.WriteLine("Student name : " + su.Name);
+            Console.WriteLine("Student ID   : " + su.ID);
+            Console.WriteLine("Student GPA  : " + su.GPA);
+            Console.ReadLine();
+        }
+    }
+    class Student
+    {
+        public string Name;
+        public string ID;
+        public float GPA;
+    }
+
+}
