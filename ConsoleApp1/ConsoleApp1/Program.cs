ff9ad1987252f6e69e91dfdf46113b1191b65747
chitsanu345 <chitsanu345@gmail.com>
2017-10-05 21:38:42 +0700

    P09

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
-    fd
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Teacher t = new Person();
+            t.SayHi();
+            Console.ReadKey();
+        }
+
+
+
+
+
+        class Person
+        {
+            public virtual void SayHi()
+            {
+                Console.WriteLine("Hi I'm a person");
+            }
+        }
+
+        class Student : Person
+        {
+            public override void SayHi()
+            {
+                Console.WriteLine("Hi I'm a student");
+            }
+        }
+
+        class Teacher : Person
+        {
+            public new void SayHi()
+            {
+                Console.WriteLine("Hi I'm a teacher");
+            }
+        }
+    }
+}
